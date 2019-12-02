12a4b7ff53c6c64a887fe3e620b22bb0d7890fa5
Colin Jaffe <balloonasaurus@gmail.com>
2019-12-02 12:25:26 -0500

    Removes solution.

--- main.js
+++ main.js
@@ -1,31 +1,13 @@
-const makeDino = function(species, period, carnivore, extinct = false) {
-  return {
-    species: species,
-    period: period,
-    carnivore: carnivore,
-    extinct: extinct
-  }
+const makeDino = function() {
 }
 
-const makeSingular = function(dinosaur) {
-  if (dinosaur.species.endsWith('us')) {
-    return makeDino(dinosaur.species.slice(0, -2), dinosaur.period, dinosaur.carnivore, dinosaur.extinct)
-  } else {
-    return dinosaur;
-  }
+const makeSingular = function() {
 }
 
-const truncateSpecies = function(dinosaur) {
-  const newDino = makeDino(dinosaur.species, dinosaur.period, dinosaur.carnivore, dinosaur.extinct);
-
-  newDino.species = newDino.species.length <= 10
-    ? newDino.species
-    : newDino.species.slice(0, 7) + '...';
-
-  return newDino; 
+const truncateSpecies = function() {
 }
 
-const makeExtinct = function(dinosaur) {
+const makeExtinct = function() {
 
 }
 
