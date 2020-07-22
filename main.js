ae65ee72c04f18054dafa5a48e97ef4554f0d55c
yiguo-17 <yiguo@yiguo.localdomain>
2020-07-22 17:07:58 -0400

    edit functions based on test file.

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
-const makeDino = function() {
+const makeDino = function(newSpecies,newPeriod,newDiet,existence) {
+  if(existence === undefined){existence = false}
+  return{
+    species: newSpecies,
+    period: newPeriod,
+    carnivore: newDiet,
+    extinct: existence
+  }
 }
 
-const makeSingular = function() {
+const makeSingular = function(dino) {
+  let newName = '';
+  if(dino.species.slice(-2)==='us'){newName += dino.species.slice(0,-2)}
+  else {newName = dino.species}
+  return{
+    species: newName,
+    period: dino.period,
+    carnivore: dino.carnivore,
+    extinct: dino.extinct
+  }
 }
 
-const truncateSpecies = function() {
+const truncateSpecies = function(dino) {
+  let newName = '';
+  if(dino.species.length>7){newName += dino.species.slice(0,7)+'...'}
+  else {newName += dino.species}
+  return{
+    species: newName,
+    period: dino.period,
+    carnivore: dino.carnivore,
+    extinct: dino.extinct
+  }
 }
 
-const makeExtinct = function() {
-
+const makeExtinct = function(dino) {
+  return{
+    species: dino.species+'...',
+    period: dino.period,
+    carnivore: dino.carnivore,
+    extinct: true
+  }
 }
 
 module.exports = {
